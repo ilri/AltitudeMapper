61da8a4897b1b9e1b817399f421519a8ff39c45a
GKosgei <mr.gkosgei@gmail.com>
2023-08-23 11:40:46 +0300

    process complete 1.Get locations 2.Break locations into small chucks for processing 3.Get elevation details based on location details 4. Save elevation data

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # altitude-generator
+
+## set environment variables
+export DB_SCHEMA={insert here}
+export DB_USER={insert here}
+export DB_PASSWORD={insert here}
+export DB_GOOGLE_API_KEY={insert here}
--- main.py
+++ main.py
@@ -19,42 +19,69 @@ class Process:
 
 
 
+    # The records are many. The function below break the records into small manageable chunks
+    def chunk(self,input_list, chunk_size):
+        return [input_list[i:i + chunk_size] for i in range(0, len(input_list), chunk_size)]
 
+    def check_string_value(self,input_string):
+        if input_string:  # This checks if the string is not empty or None
+            return True
+        else:
+            return False
 
     def execute(self):
-
-
         try:
-            print(self.host)
-            connection = mysql.connector.connect(host=self.host, database=self.database, user=self.user,
+            print('Establish DB Connection')
+            db_connection = mysql.connector.connect(host=self.host, database=self.database, user=self.user,
                                                  password=self.password)
 
-            if connection.is_connected():
-                sql_select_query = "select id,longitude,latitude from reports.et_farm_gps order by id"
-                cursor = connection.cursor()
+            gmaps = googlemaps.Client(key=self.google_api_key)
+
+
+            if db_connection.is_connected():
+                print('DB Connection Successful')
+                print('Fetching Data Points')
+                sql_select_query = "SELECT latitude,longitude FROM reports.et_farm_gps ORDER BY id"
+                cursor = db_connection.cursor()
                 cursor.execute(sql_select_query)
                 records = cursor.fetchall()  # get all records
-                print("Total Number Of Data Points: ", cursor.rowcount)
-                requests = []
-                # for record in records:
-                #     record_id = record[0]
-                #     longitude = record[1]
-                #     latitude = record[2]
-                #
-                #
-                #     sql_update_query = "UPDATE data_points SET is_processed =1 WHERE trim(id)={id}".format(
-                #         id=record_id)
-                #
-                #
-                #     cursor.execute(sql_update_query)
-                #     connection.commit()
+                print(cursor.rowcount,'Data points Returned')
+
+                chunk_size = 500
+                chunk_count = cursor.rowcount//chunk_size
+                counter = 0;
+                # Split the records into smaller groups
+
+                print('Splitting Records Into Small Chunks')
+                chunks = self.chunk(records, chunk_size)
+                print('Splitting Complete')
+
+                # Iterate through each small chunk
+                for chunk in chunks:
+                    counter+=counter+1
+                    print ('Processing Chunk')
+                    elevation_results = gmaps.elevation(chunk)
+
+                    # Define the SQL query to insert data into the database
+                    insert_query = "INSERT INTO reports.et_elevation_data (elevation, latitude, longitude) VALUES (%s, %s, %s)"
+
+                    # Extract values from the data and create a list of tuples
+                    insert_data = [(elevation_result['elevation'], elevation_result['location']['lat'], elevation_result['location']['lng']) for elevation_result in
+                                   elevation_results]
+
+                    # Execute the insert query with the list of tuples
+                    cursor.executemany(insert_query, insert_data)
+                    db_connection.commit()
+
 
         except Error as e:
             print("Error while connecting to MySQL", e)
         finally:
-            if connection.is_connected():
+            print('Closing DB Connection')
+            if db_connection.is_connected():
                 cursor.close()
-                connection.close()
+                db_connection.close()
+                print('DB Connection Closed')
 
 
 if __name__ == '__main__':
